843ed841df5c2c1e1539dcb24bcfa45a7c111163
Oluwatobi Oyejide <moyejide2016@gmail.com>
2020-04-29 13:11:21 +0100

    added solution to doomsday

---
+++ doomsday.js
@@ -0,0 +1,20 @@
+const solution = (area) =>{
+  let squareArray = [];
+  const calcSqrs = (area) => {
+    let root = Math.trunc(Math.sqrt(area));
+    let square = root * root;
+    squareArray.push(square);
+    if(square == area){
+      return;
+    }else if(area - square == 1){
+      squareArray.push(1);
+      return;
+    }else if(area - square > 1){
+      calcSqrs((area - square))
+    }
+  }
+  calcSqrs(area);
+  return squareArray;
+}
+
+console.log(solution(15324));
